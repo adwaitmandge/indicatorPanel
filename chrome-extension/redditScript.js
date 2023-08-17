5e71a8b0bb2dd529dab2656fb661c26ef7d0e7ca
Firefist01 <adwaitmandge@gmail.com>
2023-08-17 19:07:11 +0530

    Reddit and Instascript added

--- chrome-extension/redditScript.js
+++ chrome-extension/redditScript.js
@@ -5,63 +5,10 @@ function manipulateDOM() {
   console.log("hoisfaj");
   // Example: Changing the background color of all paragraphs to red
 
-  var divChild = document.createElement("div");
-  divChild.style.border = "16px solid #f3f3f3";
-  divChild.style.borderTop = "16px solid #3498db";
-  divChild.style.borderRadius = "50%";
-  divChild.style.width = "40px";
-  divChild.style.height = "40px";
-  divChild.style.animation = "spin 2s linear infinite";
-
-  // Create the @keyframes spin animation
-  var styleSheet = document.styleSheets[0];
-  var keyframes = `@keyframes spin {
-0% { transform: rotate(0deg); }
-100% { transform: rotate(360deg); }
-}`;
-
-  if (styleSheet.insertRule) {
-    styleSheet.insertRule(keyframes, styleSheet.cssRules.length);
-  } else if (styleSheet.addRule) {
-    styleSheet.addRule("@keyframes spin", keyframes);
-  }
-
-  // Append divChild to spanParent
-  var spanParent = document.createElement("span");
-  spanParent.appendChild(divChild);
-  imageElement.parentElement.appendChild(spanParent);
-
-  setInterval(() => {
-    const postContainer = document
-      .querySelector("main")
-      .querySelector("article")
-      .querySelector("div")
-      .children.item(2)
-      .querySelector("div")
-      .querySelector("div")
-      .children.item(2)
-      .querySelector("div")
-      .children.item(1)
-      .querySelector("span");
-    console.log("About to display the element");
-
-    console.log(postContainer.innerText);
-  }, 4000);
-  // tweet.style.backgroundColor = "red";
-}
-
-const extractImages = async () => {
-  console.log("Inside the extractImages function");
-
   setTimeout(async () => {
-    const imagesContainer = document
-      .querySelector("main")
-      .querySelector("div")
-      .children.item(3)
-      .querySelectorAll("img");
-
-    for (const imageElement of imagesContainer) {
-      console.log(imageElement.src);
+    const allHeadlines = document.querySelectorAll("h3");
+    for (const headline of allHeadlines) {
+      console.log(headline.innerText);
 
       // Creating spinner
       var divChild = document.createElement("div");
@@ -88,12 +35,11 @@ const extractImages = async () => {
       // Append divChild to spanParent
       var spanParent = document.createElement("span");
       spanParent.appendChild(divChild);
-      imageElement.parentElement.appendChild(spanParent);
-
+      headline.parentElement.appendChild(spanParent);
       try {
-        const res = await fetch("http://localhost:5000/imagegenerator", {
+        const res = await fetch("http://localhost:5000/category_classifier", {
           method: "POST",
-          body: imageElement.src,
+          body: headline.innerText,
         });
 
         const data = await res.json();
@@ -112,7 +58,6 @@ const extractImages = async () => {
         };
 
         console.log(newData);
-
         if (newData.newsType != "factual news") {
           // article.style.backgroundColor = "red";
           spanParent.style.display = "none";
@@ -120,15 +65,14 @@ const extractImages = async () => {
           paragraph.innerHTML = "&#10006;";
           paragraph.style.fontSize = "40px";
 
-          console.log(imageElement.parentElement);
-          imageElement.parentElement.parentElement.appendChild(paragraph);
+          headline.parentElement.appendChild(paragraph);
         } else {
           spanParent.style.display = "none";
           const paragraph = document.createElement("p");
           paragraph.innerHTML = "&#9989;";
           paragraph.style.fontSize = "40px";
 
-          imageElement.parentElement.parentElement.appendChild(paragraph);
+          headline.parentElement.appendChild(paragraph);
         }
 
         try {
@@ -143,16 +87,27 @@ const extractImages = async () => {
         } catch (err) {
           console.error(err.message);
         }
+
+        // if (
+        //   data.result.splice(4) == "Fake" ||
+        //   data.result.splice(4) == "fake" ||
+        //   data.result.splice(4) == 0
+        // ) {
+        // } else {
+        //   return;
+        // }
       } catch (err) {
         console.error(err.message);
       }
+
+      headline.parentElement.appendChild(spanParent);
     }
-  }, 8000);
-};
+  }, 4000);
+  // tweet.style.backgroundColor = "red";
+}
 
 // Execute the function when the content script is injected into the page
-// manipulateDOM();
-extractImages();
+manipulateDOM();
 
 // async function apicall() {
 //   const res = await fetch("https://dummyjson.com/products");
